ac56686f6397313bfe9757cc97b48254ecc7a4dc
LegitBaboulinet <inscriptions@lucasmacori.fr>
2019-08-29 09:08:09 +0200

    (feature) Utilisation des formulaires

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,13 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index()
+    {
+        // Redirection vers la page de gestion des utilisateurs
+        return $this->redirect("users");
+    }
+
+    /**
      * @Route("/users", name="users", methods={"GET"})
      */
-    public function index(Request $request)
+    public function users(Request $request)
     {
         // Récupération des utilisateurs
         $repository = $this->getDoctrine()->getRepository(AppUser::class);
 
+        // Récupération des paramètres GET
         $success = $request->query->get('success');
         $message = $request->query->get('message');
 
@@ -28,97 +38,103 @@ class UserController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/user/new", name="displayCreate", methods={"GET"})
-     */
-    public function displayCreate()
+    private function getForm(AppUser $user, string $action)
     {
-        // Affichage de la page de création
-        return $this->render('user/create.html.twig');
+        // Création du formulaire
+        return $this->createFormBuilder($user)
+            ->add('firstName', 'Symfony\Component\Form\Extension\Core\Type\TextType', [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', 'Symfony\Component\Form\Extension\Core\Type\TextType', [
+                'label' => 'Nom de famille'
+            ])
+            ->add('email', 'Symfony\Component\Form\Extension\Core\Type\EmailType', [
+                'label' => 'Adresse email'
+            ])
+            ->add('location', 'Symfony\Component\Form\Extension\Core\Type\TextType', [
+                'label' => 'Ville'
+            ])
+            ->add('age', 'Symfony\Component\Form\Extension\Core\Type\NumberType', [
+                'label' => 'Âge'
+            ])
+            ->add('submit', 'Symfony\Component\Form\Extension\Core\Type\SubmitType', [
+                'label' => $action
+            ])
+            ->getForm();
     }
 
     /**
-     * @Route("/user/new", name="create", methods={"POST"})
+     * @Route("/user/new", name="create", methods={"GET", "POST"})
      */
     public function create(Request $request)
     {
-        $success = false;
-        try {
-            $manager = $this->getDoctrine()->getManager();
+        // Création du formulaire
+        $user = new AppUser();
+        $form = $this->getForm($user, 'Créer l\'utilisateur');
 
-            // Création de l'utilisateur
-            $user = new AppUser();
-            $user->setFirstName($request->get('firstName'));
-            $user->setLastName($request->get('lastName'));
-            $user->setEmail($request->get('email'));
-            $user->setLocation($request->get('location'));
-            $user->setAge($request->get('age'));
-            $user->setDate(new \DateTime());
+        $form->handleRequest($request);
 
-            // Sauvegarde de l'utilisateur
-            $manager->persist($user);
-            $manager->flush();
+        // Vérification des données
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                // Récupération de la connexion à la base de données
+                $em = $this->getDoctrine()->getManager();
+                $user->setDate(new \DateTime());
 
-            $success = true;
-        } catch (Exception $e) {
-            $success = 1;
+                // Sauvegarde de l'utilisateur
+                $em->persist($user);
+                $em->flush();
+                $success = true;
+                $message = 'L\'utilisateur ' . $user->getFirstName() . ' ' . $user->getLastName() . ' a bien été créé';
+            } catch (Exception $e) {
+                $success = false;
+                $message = 'Une erreur est survenue lors de la création de l\'utilisateur';
+            }
+
+            // On redirige vers la page de visualisation de l'annonce nouvellement créée
+            return $this->redirect($this->generateUrl('users'));
         }
 
-        // Redirection vers la page d'accueil
-        return $this->redirect('/users?success=' . $success . '&message=L\'utilisateur a bien été créé');
+        // Affichage de la page de création
+        return $this->render('user/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
-     * @Route("/user/{id}", name="displayUpdate", methods={"GET"})
-     */
-    public function displayUpdate(int $id)
-    {
-        // Récupération de l'utilisateur
-        $repository = $this->getDoctrine()->getRepository(AppUser::class);
-        $user = $repository->find($id);
-
-        if ($user != null) {
-            // Affichage de la page de modification
-            return $this->render('user/create.html.twig', [
-                'user' => $user
-            ]);
-        } else {
-            return $this->render('404.html.twig');
-        }
-    }
-
-    /**
-     * @Route("/user/{id}", name="update", methods={"POST"}, requirements={"page"="\d+"})
+     * @Route("/user/{id}", name="update", methods={"GET", "POST"})
      */
     public function update(int $id, Request $request)
     {
-        $success = false;
-        try {
-            $manager = $this->getDoctrine()->getManager();
-            $repository = $this->getDoctrine()->getRepository(AppUser::class);
+        // Récupération de la connexion à la base de données
+        $repository = $this->getDoctrine()->getRepository(AppUser::class);
 
-            // Création de l'utilisateur
-            $user = $repository->find($id);
-            if ($user == null) {
-                return $this->render('404.html.twig');
-            }
-            $user->setFirstName($request->get('firstName'));
-            $user->setLastName($request->get('lastName'));
-            $user->setEmail($request->get('email'));
-            $user->setLocation($request->get('location'));
-            $user->setAge($request->get('age'));
+        // Récupération de l'utilisateur avec son id
+        $user = $repository->find($id);
+
+        // Création du formulaire
+        $form = $this->getForm($user, 'Modifier l\'utilisateur');
+
+        $form->handleRequest($request);
+
+        // Vérification des données
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager = $this->getDoctrine()->getManager();
 
             // Sauvegarde de l'utilisateur
             $manager->persist($user);
             $manager->flush();
 
-            $success = true;
-        } catch (Exception $e) {
-            $success = 1;
+            // Redirection de l'utilisateur vers la page d'accueil
+            return $this->redirect($this->generateUrl('users'));
         }
 
-        // Redirection vers la page d'accueil
-        return $this->redirect('/users?success=' . $success . '&message=L\'utilisateur a bien été modifié');
+        // Affichage de la page de création
+        return $this->render('user/create.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user
+        ]);
     }
 
     /**
@@ -127,6 +143,7 @@ class UserController extends AbstractController
     public function delete(int $id, Request $request)
     {
         try {
+            // Récupération de la connexion à la base de données
             $manager = $this->getDoctrine()->getManager();
             $repository = $this->getDoctrine()->getRepository(AppUser::class);
 
